7ce8edc89959d1fdc77189f428a1b05a73cd8a2a
Alex Dadukin <ald.games.2014@outlook.com>
2016-04-09 18:10:25 +0300

    Some beatify fixes

--- index.js
+++ index.js
@@ -1,76 +1,67 @@
-// Dependencies
-var url  = require('url');
-var path = require('path');
-var mime = require('mime');
-var fs = require('fs');
-var zlib = require('zlib');
-var cache = {}
+'use strict';
 
-module.exports = function(options){
-	return function($){
-		var pathname = $.url.pathname;
-		var mimeType = mime.lookup(pathname);
-		var extension = path.extname(pathname);
-		var dateOffset = 604800000;
-		
-		// if no route was specified there is an extension and mimeType is not binary
-		if(extension){
-			// set header
-			$.header('Content-Type', mimeType);
-			$.status(200);
-			var source = options.path + $.url.pathname;
-			fs.stat(source, function (error, stats) {
-				if(!error){
-					$.header('Last-Modified', new Date(stats.mtime).toUTCString())
-					$.header('Expires', new Date(new Date().getTime() + dateOffset).toUTCString())
-					$.header('Cache-Control', 'public')
-					var modified_since = new Date($.headers['if-modified-since']).getTime();
-					var last_modified = new Date(stats.mtime).getTime()
-					if(!$.headers['if-modified-since'] || last_modified > modified_since){
-						
-						// file was modified
-						fs.readFile(source, function(readerror, data){
-							if(readerror) throw readerror
-							
-							if(mimeType == 'text/css' || mimeType == 'application/javascript'){	
-								var buffer = new Buffer(data);
-								zlib.gzip(buffer, function(error, gzip){
-									if(error) throw error
-									cache[source] = gzip;
-									$.header('Content-Encoding', 'gzip')
-									$.header('Vary', 'Accept-Encoding')
-									$.passed = false;
-									$.responded = true;
-									$.response.end(gzip)
-									$.return()
-								})
-							} else {
+const fs = require('fs');
+const url = require('url');
+const path = require('path');
+const mime = require('mime');
+const zlib = require('zlib');
+
+const cache = {};
+const DATE_OFFSET = 604800000;
+
+module.exports = options => $ => {
+	const pathname = $.url.pathname;
+	const mimeType = mime.lookup(pathname);
+	const extension = path.extname(pathname);
+
+	if (extension) {
+		$.header('Content-Type', mimeType);
+		$.status(200);
+		const source = options.path + $.url.pathname;
+		fs.stat(source, (error, stats) => {
+			if (!error) {
+				$.header('Last-Modified', new Date(stats.mtime).toUTCString());
+				$.header('Expires', new Date(new Date().getTime() + DATE_OFFSET).toUTCString());
+				$.header('Cache-Control', 'public');
+				const modifiedSince = new Date($.headers['if-modified-since']).getTime();
+				const lastModified = new Date(stats.mtime).getTime();
+				if (!$.headers['if-modified-since'] || lastModified > modifiedSince) {
+
+					fs.readFile(source, (readerror, data) => {
+						if (readerror) throw readerror;
+						if (mimeType === 'text/css' || mimeType === 'application/javascript') {
+							const buffer = new Buffer(data);
+							zlib.gzip(buffer, (error, gzip) => {
+								if (error) throw error;
+								cache[source] = gzip;
+								$.header('Content-Encoding', 'gzip');
+								$.header('Vary', 'Accept-Encoding');
 								$.passed = false;
 								$.responded = true;
-								$.response.end(data)
-								$.return()
-							}
-						})
-					
-					} else {
-						// not modified
-						$.status(304)
-						$.responded = true;
-						$.response.end()
-						$.return()
-					}
-					
-				} else if (error.type != 'ENOENT') {
-					$.status(error.status || 500, 'File not found');
-					$.return();
+								$.response.end(gzip);
+								$.return();
+							});
+						} else {
+							$.passed = false;
+							$.responded = true;
+							$.response.end(data);
+							$.return();
+						}
+					});
 				} else {
-					throw error;
-					$.return()
+					$.status(304);
+					$.responded = true;
+					$.response.end();
+					$.return();
 				}
-				
-			});
-		} else {
-			$.return();
-		}
+			} else if (error.type !== 'ENOENT') {
+				$.status(error.status || 500, 'File not found');
+				$.return();
+			} else {
+				throw error;
+			}
+		});
+	} else {
+		$.return();
 	}
-}
+};
